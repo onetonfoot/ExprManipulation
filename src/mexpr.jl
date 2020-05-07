4eadb2c8b58bee87174b1058603a396d1bc42ab0
Dom <onetonfoot@gmail.com>
2020-05-07 12:19:45 +0800

    get basic pre and postorder working

--- src/mexpr.jl
+++ src/mexpr.jl
@@ -1,115 +1,121 @@
 import Base: ==, match
 using Base.Meta: show_sexpr
 
-struct MExpr
-    head::Union{Symbol,AbstractCapture,AbstractTransform}
+struct MExpr{K} 
+    head::Symbol
     args::Array{Any}
 
-    function MExpr(head::Union{Symbol,AbstractCapture,AbstractTransform}, args::Array)
-        seen_capture = false
-        for (idx, arg) in enumerate(args)
-            if arg isa SplatCapture && idx != length(args)
-                throw(ArgumentError("SplatCapture can only be used as final argument in MExpr"))
+    function MExpr(head::Symbol, args::Array)
+        seen_slurp = false
+        n_slurps = 0
+        # TODO convert exprssions inside to MExpr
+        for arg in args
+            if arg isa Slurp
+                n_slurps += 1
             end
         end
-        new(head, args)
+        if n_slurps > 1
+            error("Only one slurp allow per an expression")
+        end
+        new{head}(head, args)
     end
 end
 
-MExpr(head) = MExpr(head, [])
-MExpr(head, arg) = MExpr(head, [arg])
-MExpr(head, arg, vargs...) = MExpr(head, [arg, vargs...])
-match_capture = MExpr(:call, :Capture, :_)
-
-poparg!(m_expr::MExpr, array::Array) = popfirst!(array) 
-poparg!(capture::Capture{N}, array::Array) where N =  length(array) < N ? nothing : [popfirst!(array) for i in 1:N]
-function poparg!(capture::SplatCapture, array::Array)
-    values = []
-    for i in 1:length(array)
-        push!(values, popfirst!(array))
-    end
-    values
-end
+MExpr(head::Symbol)  =  MExpr(head, [])
+MExpr(head, args...) where {K} = MExpr(head, collect(args))
 
 (==)(match_expr::MExpr, expr::Expr) = (==)(expr, match_expr)
 (==)(match_expr::MExpr, x) = false
-(==)(x, match_expr::MExpr) = false
 
-function (==)(expr::Expr, match_expr::MExpr) 
+Base.match(capture::Capture, arg) = capture.fn(arg) ? Dict(capture.key => arg) : nothing
+# Base.match(capture::SplatCapture, args::Array) = capture.fn(args) ? Dict(capture.val => args) : nothing
 
-    if match_expr.head != expr.head
-        return false
-    end
 
-    total_n = 0
-    n_args = length(expr.args)
-    expr = deepcopy(expr)
-    match_expr = deepcopy(match_expr)
+haschildren(expr::Union{Expr,MExpr}) = !isempty(expr.args)
+haschildren(x) = false
+children(expr::Union{Expr,MExpr}) = expr.args
+children(x) = []
 
-    # TODO add support for :_
-    while !isempty(expr.args) && !isempty(match_expr.args)
-        match_arg = popfirst!(match_expr.args)
-        match_arg = match_arg isa AbstractTransform ? match_arg.capture :  match_arg
-        if isnothing(match_arg)
-            # TODO better error message
-            error("Transform has no associated capture so can't be used yet")
-        end
-        if match_arg isa Capture
-            args = poparg!(match_arg, expr.args)
-            if isnothing(args)
-                return false
-            elseif all(match_arg.fn.(args))
-                total_n += length(args)
-            else
-                return false
-            end
-        elseif match_arg isa SplatCapture
-            arg = poparg!(match_arg, expr.args)
-            if match_arg != arg
-                return false
-            end
-            total_n += length(arg)
-        elseif match_arg == popfirst!(expr.args)
-            total_n += 1
+
+# https://stackoverflow.com/questions/55606017/postorder-traversal-of-an-n-ary-tree
+# https://www.geeksforgeeks.org/iterative-postorder-traversal-of-n-ary-tree/?ref=leftbar-rightbar
+function postorder(root::MExpr)
+    stack = Any[root]
+    last_child =  nothing
+
+    while !isempty(stack)
+        root = stack[end]
+        # node has no child, or one child has been visted, the process and pop it
+        if !haschildren(root) || (!isnothing(last_child) &&  last_child in children(root))
+            println(root)
+            pop!(stack)
+            last_child = root
         else
-            return false
+            append!(stack, reverse(root.args))
         end
     end
-    total_n == n_args
 end
 
-Base.match(capture::Capture{1}, expr::Array) = Dict(capture.val => expr[1])
-Base.match(capture::Capture{N}, expr::Array) where N = Dict(capture.val => expr)
-Base.match(capture::SplatCapture, expr::Array) = Dict(capture.val => expr)
-Base.match(transform::AbstractTransform, expr) = match(transform.capture, expr)
+function preorder(root::MExpr)
+    stack = Any[root]
+    last_child =  nothing
+
+    while !isempty(stack)
+        root = pop!(stack)
+        println(root)
+        # node has no child, or one child has been visted, the process and pop it
+        if !haschildren(root) || (!isnothing(last_child) &&  last_child in children(root))
+            # LOGIC HERE
+            last_child = root
+        else
+            append!(stack, reverse(root.args))
+        end
+    end
+end
 
 function Base.match(match_expr::MExpr, expr::Expr) 
 
     values = []
 
-    if match_expr != expr
+    if match_expr.head != expr.head
         return nothing
     end
 
-    if match_expr.head isa Union{AbstractCapture,AbstractTransform}
-        push!(values, match(match_expr.head, [expr.head]))
-    end
-        
-    # don't think this has to be a deepcopy just a copy but need to define copy for MExpr
-    expr = deepcopy(expr)
-    match_expr = deepcopy(match_expr)
+    total = 0
 
-    while !isempty(expr.args) && !isempty(match_expr.args)
-        match_arg = popfirst!(match_expr.args)
-        if match_arg isa Union{AbstractCapture,AbstractTransform,MExpr}
-            match_arg = match_arg isa AbstractTransform ? match_arg.capture :  match_arg
-            result = match(match_arg, poparg!(match_arg, expr.args))
-            push!(values, result)
-        else
-            popfirst!(expr.args)
+    e_idx = 1
+    m_idx = 1
+
+    while m_idx <= length(match_expr.args) && e_idx <= length(expr.args)
+        arg = expr.args[e_idx]
+        m_arg = match_expr.args[m_idx]
+        @show m_arg
+        @show arg
+        if m_arg isa Capture
+            matches = match(m_arg, arg)
+            if isnothing(matches)
+                return nothing
+            end
+            push!(values, matches)
+        # TODO add elseif for slurp
+        # elseif m_arg isa Slurp
+        #     #
+        #     # length(expr.args) - total
+        #     args = []
+        #     s_idx = e_idx
+        #     # while 
+
         end
+        total += 1
+        m_idx += 1
+        e_idx += 1
     end
-    return merge(values...)
+
+    if total != length(expr.args)
+        nothing
+    else
+        merge(values...)
+    end
 end
 
 function transform(m_expr::MExpr, expr::Expr)
@@ -144,21 +150,22 @@ function transform(m_expr::MExpr, expr::Expr)
     Expr(head, args...)
 end
 
-function create_expr(match_expr::MExpr, replace_capture)
-    head = match_expr.head
-    expr =  Expr(head isa Symbol ? head : :_)
-    for arg in match_expr.args
-        arg = if arg isa Capture && replace_capture
-            :_
-        elseif arg isa MExpr
-            create_expr(arg, replace_capture)
-        else
-            arg
-        end
-        push!(expr.args, arg)
-    end
-    expr
-end
 
-Base.show(io::IO, expr::MExpr) = show(create_expr(expr, true))
-Meta.show_sexpr(expr::MExpr) = show_sexpr(create_expr(expr, false))
+# function create_expr(match_expr::MExpr, replace_capture)
+#     head = match_expr.head
+#     expr =  Expr(head isa Symbol ? head : :_)
+#     for arg in match_expr.args
+#         arg = if arg isa Capture && replace_capture
+#             :_
+#         elseif arg isa MExpr
+#             create_expr(arg, replace_capture)
+#         else
+#             arg
+#         end
+#         push!(expr.args, arg)
+#     end
+#     expr
+# end
+
+# Base.show(io::IO, expr::MExpr) = show(create_expr(expr, true))
+# Meta.show_sexpr(expr::MExpr) = show_sexpr(create_expr(expr, false))
